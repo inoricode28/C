f0bfefff55e68eace4a9f280944f77deb1bf4acf
inoricode28 <inori237@gmail.com>
2021-01-29 02:30:05 -0500

    C03

--- ventas.cpp
+++ ventas.cpp
@@ -4,27 +4,18 @@
 
 using namespace std;
 string nombre, pass, correo, web;
-float cambio,placa,procesador,ram,hdd,video,chasis,monitor,dvd,teclamouse,estabilizador;
+float placa,procesador,ram,hdd,video,chasis,monitor,dvd,teclamouse,estabilizador,precio,igv=0.18,resultado,cambio;
 
 
 void escribir()
 {
  ofstream archivo;
  archivo.open("Deltron.txt",ios::out);
-
- /*if (archivo.fail())
- 	{
- 		cout<<"No se pudo abrir el archivo";
- 		exit(1);
-	}*/
-
  archivo << "Tipo de cambio: " <<  cambio << endl;
  archivo << "Placa Madre :" << placa << endl;
  archivo << "Procesador 'CPU' :" << procesador << endl;
  archivo << "Memoria 'RAM' :" << ram << endl;
  archivo << "Disco Duro :" << hdd << endl;
- cout << "El registro de usuario se ha realizado satisfactoriamente,"
-    "debe abrir el archivo Registro.txt en la carpeta del programa para ver los datos almacenados." << endl;
 
 	archivo.close();
 }
@@ -46,11 +37,17 @@ int main()
  cout << "Monitor:\t\t";  cin >> monitor;
  cout << "Otros";
  cout << "\n";
- cout << "- Unidad Optica:\t";  cin >> dvd;
- cout << "- Teclado / Mouse:\t";  cin >> teclamouse;
- cout << "- Estabilizador:\t";  cin >> estabilizador;
-
-	
+ cout << ">>> Unidad Optica:\t";  cin >> dvd;
+ cout << ">>> Teclado / Mouse:\t";  cin >> teclamouse;
+ cout << ">>> Estabilizador:\t";  cin >> estabilizador;
+ cout << "\n";
+ precio = ((placa+procesador+ram+hdd+video+chasis+monitor+dvd+teclamouse+estabilizador)*cambio);
+ resultado=(precio*igv)+precio;
+ cout << "Precio Neto en soles inc. IGV:\t"<<resultado<<endl;
+ cout << "\n";
+ cout << "El registro de usuario se ha realizado satisfactoriamente,"
+    "debe abrir el archivo Registro.txt en la carpeta del programa para ver los datos almacenados." << endl;
+ 
 	escribir();
  
  return 0;
